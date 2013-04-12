02e5a81830ce3965408e39d0ec8eed7fcfc2e050
Victor J <victor.reventos1@gmail.com>
2013-04-12 12:46:53 -0400

    Using the new Resources class

--- src/com/github/pageallocation/gui/PropertiesWindow.java
+++ src/com/github/pageallocation/gui/PropertiesWindow.java
@@ -9,7 +9,6 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
