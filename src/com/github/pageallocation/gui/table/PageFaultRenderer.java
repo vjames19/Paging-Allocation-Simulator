223eb4ad59c871173a46533f5ea57052128a7d75
Victor J <victor.reventos1@gmail.com>
2013-04-22 14:45:02 -0400

    Update to depict the page fault only when the reference appears on the cell.

--- src/com/github/pageallocation/gui/table/PageFaultRenderer.java
+++ src/com/github/pageallocation/gui/table/PageFaultRenderer.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
 import com.github.pageallocation.util.Util;
@@ -13,11 +14,15 @@ import com.github.pageallocation.util.Util;
  * Renderer used to depict a Page Fault. If the current reference was not in the
  * previous column then a Page Fault occurred.
  * 
- * <p> For this render to not depict the columns as a page fault, the header
- * of the table must be other than an integer. That way the renderer will not color 
- * any column of the table.
+ * <p>
+ * For this render to not depict the columns as a page fault, the header of the
+ * table must be other than an integer. That way the renderer will not color any
+ * column of the table.
  * 
- * <p> It will only color columns with the header being an integer. If a page fault occurs.
+ * <p>
+ * It will only color columns with the header being an integer. If a page fault
+ * occurs.
+ * 
  * @author Victor J.
  * 
  */
@@ -28,7 +33,8 @@ public class PageFaultRenderer extends DefaultTableCellRenderer {
 			boolean isSelected, boolean hasFocus, int row, int column) {
 		Component renderer = super.getTableCellRendererComponent(table, value,
 				isSelected, hasFocus, row, column);
-		boolean pageFault = pageFaultOccurred(table.getModel(), column);
+		boolean pageFault = pageFaultOccurred(
+				(DefaultTableModel) table.getModel(), column);
 		if (pageFault) {
 			renderer.setBackground(Color.CYAN);
 		} else {
@@ -37,7 +43,8 @@ public class PageFaultRenderer extends DefaultTableCellRenderer {
 		return renderer;
 	}
 
-	private boolean pageFaultOccurred(final TableModel model, final int column) {
+	private boolean pageFaultOccurred(final DefaultTableModel model,
+			final int column) {
 		String columnName = model.getColumnName(column);
 
 		if (column == 0) {// Frames column
@@ -53,23 +60,31 @@ public class PageFaultRenderer extends DefaultTableCellRenderer {
 				return false;
 			}
 			/*
-			 * Search the value in the past column. If the value is not in the past
-			 * column then a page fault occurred.
+			 * Search the value in the past column. If the value is not in the
+			 * past column then a page fault occurred.
 			 */
 			int searchColumn = column - 1;
-			for (int i = 0; i < rows; i++) {// Search the value
-				Object value = model.getValueAt(i, searchColumn);
-				if (value == null) {
-					continue;
-				} else {
-					String v = (String) value;
-					if (columnName.equals(v)) {// found it, no page fault
-						return false;
-					}
+
+			return (!columnContains(model, rows, searchColumn, columnName) && columnContains(
+					model, rows, column, columnName));
+		}
+
+		return false;
+
+	}
+
+	private boolean columnContains(DefaultTableModel model, int rows,
+			int column, String columnName) {
+		for (int i = 0; i < rows; i++) {// Search the value
+			Object value = model.getValueAt(i, column);
+			if (value == null) {
+				continue;
+			} else {
+				String v = (String) value;
+				if (columnName.equals(v)) {// found it
+					return true;
 				}
 			}
-			
-			return true;
 		}
 
 		return false;
@@ -78,6 +93,7 @@ public class PageFaultRenderer extends DefaultTableCellRenderer {
 
 	/**
 	 * Verifies if a column is empty. Either all columns are "" or null.
+	 * 
 	 * @param model
 	 * @param column
 	 * @param rows
